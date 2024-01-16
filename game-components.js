d862efccb2f37f62738e839365300d6205cf157c
PierreENOlivier <10992175+PierreENOlivier@users.noreply.github.com>
2024-01-16 11:26:19 +0100

    Initial commit

---
+++ game-components.js
@@ -0,0 +1,11 @@
+var min = 00;
+var sec = 00;
+var appendMin = document.getElementById("min")
+var appendSec = document.getElementById("sec")
+var startGame = document.getElementById('button-start');
+
+function Timer () {
+
+}
+
+/* Button Play Again */
